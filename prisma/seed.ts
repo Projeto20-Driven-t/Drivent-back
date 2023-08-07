d9312342423c6ad278bda2da8ada26ccc140ba39
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-08-07 17:14:36 -0300

    feat: create hotel and room seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+import { Hotel, PrismaClient } from "@prisma/client";
 import dayjs from "dayjs";
+import hotels from "./hotels";
+import { createRooms } from "./rooms";
 const prisma = new PrismaClient();
 
 async function main() {
@@ -18,8 +20,55 @@ async function main() {
 
   console.log({ event });
 }
+type CreateManyHotels = {
+  name: string;
+  image: string;
+}
+async function hotelsSeed() {
+  for(let hotel of hotels) {
+    await prisma.hotel.create({
+      data: hotel
+    })
+  }
+}
+
+async function RoomsSeed() {
+  const hotels = await prisma.hotel.findMany({});
+  if(!hotels) {
+    await prisma.hotel.createMany({
+      data: [
+        {
+          name: "Driven Resort",
+          image: "https://ibb.co/WDC4D4h",
+        },
+        {
+          name: "Driven Palace",
+          image: "https://ibb.co/vQtR9Dd",
+        },
+        {
+          name: "Driven World",
+          image: "https://ibb.co/g9GJ2Xy"
+        },
+      ]
+    })
+  }
+}
 
 main()
+hotelsSeed()
+async function getHotelsList(): Promise<Hotel[]> {
+  return await prisma.hotel.findMany()
+}
+
+async function roomsSeed(){
+  const hotelsList = await getHotelsList()
+  for(let hotel of hotelsList) {
+    await createRooms(hotel.id);
+  }
+}
+
+roomsSeed()
+
   .catch((e) => {
     console.error(e);
     process.exit(1);
