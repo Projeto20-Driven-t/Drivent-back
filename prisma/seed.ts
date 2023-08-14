0aac2a8a74d442e0557591d48d8b65a52da71a64
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-08-14 16:23:01 -0300

    feat: count total vacancies for each Hotel

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,26 +4,6 @@ import hotels from "./hotels";
 import { createRooms } from "./rooms";
 const prisma = new PrismaClient();
 
-async function createHotelsCache() {
-  try {
-    await prisma.hotel.createMany({
-      data: [
-        {
-          name: "Hotel TesteCache 1",
-          image: "https://www.ahstatic.com/photos/c096_ho_00_p_1024x768.jpg",
-        },
-        {
-          name: "Hotel TesteCache 2",
-          image: "https://www.ahstatic.com/photos/1276_ho_00_p_1024x768.jpg",
-        },
-      ],
-    });
-
-    console.log("Hotéis criados com sucesso!");
-  } catch (error) {
-    console.error("Erro ao criar hotéis:", error);
-  }
-}
 
 async function main() {
   let event = await prisma.event.findFirst();
@@ -40,7 +20,6 @@ async function main() {
   }
 
   console.log({ event });
-  await createHotelsCache();
 }
 type CreateManyHotels = {
   name: string;
@@ -50,28 +29,6 @@ async function hotelsSeed() {
   for(let hotel of hotels) {
     await prisma.hotel.create({
       data: hotel
-    })
-  }
-}
-
-async function RoomsSeed() {
-  const hotels = await prisma.hotel.findMany({});
-  if(!hotels) {
-    await prisma.hotel.createMany({
-      data: [
-        {
-          name: "Driven Resort",
-          image: "https://ibb.co/WDC4D4h",
-        },
-        {
-          name: "Driven Palace",
-          image: "https://ibb.co/vQtR9Dd",
-        },
-        {
-          name: "Driven World",
-          image: "https://ibb.co/g9GJ2Xy"
-        },
-      ]
     })
   }
 }
--- src/repositories/hotel-repository/index.ts
+++ src/repositories/hotel-repository/index.ts
@@ -1,7 +1,42 @@
 import { prisma } from "@/config";
+import { Hotel, Prisma } from "@prisma/client";
+
 
 async function findHotels() {
-  return prisma.hotel.findMany();
+
+  return await prisma.$transaction(async (tx: Prisma.TransactionClient) => {
+    const hotels: Hotel[] = await tx.hotel.findMany({})
+
+    const hotelsVacancies = []
+
+    for(let hotel of hotels){
+      let roomsWithBookings = await tx.room.findMany({ 
+        where:{ hotelId: hotel.id }, 
+        include:{Booking: true}
+      })
+
+      let totalCapacity = roomsWithBookings.reduce(function (acc, currentValue){
+        return acc + currentValue.capacity
+      }, 0);
+
+      let bookingsForRoom = roomsWithBookings.map(room => {
+        return room.Booking.length
+      })
+      let totalBookings = bookingsForRoom.reduce(function (acc, currentValue) {
+        return acc + currentValue
+      }, 0);
+      let hotelTotalVancancy = totalCapacity - totalBookings
+
+      hotelsVacancies.push(hotelTotalVancancy)
+    }
+
+    const hotelsWithVacancies = []
+    for(let i = 0; i < hotels.length; i++){
+      hotelsWithVacancies.push({...hotels[i], vacancies: hotelsVacancies[i]})
+    }
+    return hotelsWithVacancies;
+  });
+
 }
 
 async function findRoomsByHotelId(hotelId: number) {
